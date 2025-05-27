c2d3207290f4590f3402641e8a4dd2daea7abe6d
rscassellati <rscassellati@brandeis.edu>
2025-05-27 14:32:55 -0400

    Bug testing

--- Data Cleaning.py
+++ Data Cleaning.py
@@ -85,7 +85,7 @@ elif st.session_state['uncleanedFile'] is not None:
             st.write(datetime.datetime.strptime("July", "%B"))
             
             st.write(datetime.datetime.strptime("July 17 2023", "%B %d %Y"))
-            st.write(" here")
+            st.write("1 here")
             st.write(str(type(result)))
             result = result.replace(" EDT", "")
             st.write(str(type(result)))
