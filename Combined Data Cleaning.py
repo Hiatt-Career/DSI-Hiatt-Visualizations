dea750360776ca2f73bfa75321d13194a637b906
rscassellati <rscassellati@brandeis.edu>
2025-07-09 18:51:05 -0400

    Undo change

--- Combined Data Cleaning.py
+++ Combined Data Cleaning.py
@@ -66,8 +66,7 @@ if 'combined_uncleanedFile' not in st.session_state:
     if original_data_file:
         df = pd.read_csv(original_data_file)
         st.session_state['combined_uncleanedFile'] = df
-        st.write("Hi there")
-        # st.rerun()
+        st.rerun()
 elif st.session_state['combined_uncleanedFile'] is not None:
     if 'combined_inProcessFile' not in st.session_state:
         st.session_state['combined_inProcessFile'] = st.session_state['combined_uncleanedFile']
